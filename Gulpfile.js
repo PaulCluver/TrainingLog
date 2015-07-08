4066c2936ee41edf96916d4726ece19ef7642406
Paul Cluver <paulcl@sahomeloans.com>
2015-07-08 11:02:54 +0200

    First commit

---
+++ Gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+
+gulp.task('connect', function () {
+  connect.server({
+    root: 'app/',
+    port: 8888
+  });
+});
+
+gulp.task('default', ['connect']);
+
+
